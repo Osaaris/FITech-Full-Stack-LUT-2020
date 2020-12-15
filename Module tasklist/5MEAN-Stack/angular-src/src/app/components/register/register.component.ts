2f6a47e2480499d97777f95b0798d8a41816bd22
unknown <o.p.saaristo@gmail.com>
2020-12-15 18:49:36 +0200

    loput videot done

--- Module tasklist/5MEAN-Stack/angular-src/src/app/components/register/register.component.ts
+++ Module tasklist/5MEAN-Stack/angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Required fields
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show("Please fill all fields", { cssClass: "alert-danger", timeout: 3000 });
+      return false;
+    }
+
+    // Validate email
+    if (!this.validateService.validateEmail(user)) {
+      this.flashMessage.show("Please use a valid email", { cssClass: "alert-danger", timeout: 3000 });
+      return false;
+    }
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show("You are now registered", { cssClass: "alert-success", timeout: 3000 });
+        this.router.navigate(["/login"]);
+      } else {
+        this.flashMessage.show("Something went wrong", { cssClass: "alert-danger", timeout: 3000 });
+        this.router.navigate(["/"]);
+      }
+    });
+  }
 }
